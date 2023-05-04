8c6a9b2c033425edd3bdf87e94c7e77bf897bb46
Byte-walker <mdshahidulridoy@gmail.com>
2023-05-04 12:42:00 +0600

    global layout updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { GlobalLayout } from '../layout/global_layout/GlobalLayout';
+import {HomePage} from '../components/component_page/home_page/HomePage';
 
 export const Home = () => {
   return (
     <div>
-      <GlobalLayout />
+      <GlobalLayout>
+        <HomePage />
+      </GlobalLayout>
     </div>
   );
 };
