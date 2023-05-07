93affc52049f9459f5d5e0bd57134ee688d4dd31
nishikaru <mdsajed001@gamil.com>
2023-05-07 15:55:24 +0600

    log in page beta 3

--- src/components/component_page/login_page/LoginPage.jsx
+++ src/components/component_page/login_page/LoginPage.jsx
@@ -2,24 +2,38 @@ import React, { useState } from "react";
 import "./loginPage.css";
 
 export const LoginPage = () => {
-  const [door,setDoor] = useState(false)
-  const doorHandler=()=>{
-    setDoor(!door)
-  }
-  const loginHandler=(event)=>{
+  const [door, setDoor] = useState(false);
+  const doorHandler = () => {
+    setDoor(!door);
+  };
+  const loginHandler = (event) => {
     event.preventDefault(); // prevent the page from reloading
-    const id = event.target[0].value; // get the value of the first input field
-    const password = event.target[1].value; // get the value of the second input field
-    console.log(id,password)  
-  }
+    const option = event.target[0].value; // get the value of the first input field
+    const id = event.target[1].value; // get the value of the second input field
+    const password = event.target[2].value; // get the value of the second input field
+    console.log(option, id, password);
+  };
   return (
     <div className="login__body flex center-y">
-      <div className={`login__div flex flex-col justify-between w-full ${door?'h-full p-20':'h-0 p-0'}`}>
+      <div
+        className={`login__div flex flex-col justify-between w-full ${
+          door ? "h-full p-20" : "h-0 p-0"
+        }`}
+      >
         <div className="login__head">
           <div className="login__title">Login</div>
         </div>
         <>
-          <form onSubmit={loginHandler} className="login__form flex flex-col center-y">
+          <form
+            onSubmit={loginHandler}
+            className="login__form flex flex-col center-y"
+          >
+            {/* <label htmlFor="my-dropdown">Choose an option:</label> */}
+            <select className="login__form--input" id="my-dropdown" name="my-dropdown">
+              <option value="student">Student</option>
+              <option value="teacher">Teacher</option>
+              <option value="admin">Admin</option>
+            </select>
             <input
               placeholder="ID"
               className="login__form--input"
@@ -38,10 +52,17 @@ export const LoginPage = () => {
             />
             <button className="login__form--btn">Login</button>
           </form>
-          <div className="login__footer">If you dont have an account contact admin</div>
+          <div className="login__footer">
+            If you dont have an account contact admin
+          </div>
         </>
       </div>
-      <div onClick={doorHandler} className={`login__door ${door?'hidden':null}`}>Login</div>
+      <div
+        onClick={doorHandler}
+        className={`login__door ${door ? "hidden" : null}`}
+      >
+        Login
+      </div>
     </div>
   );
 };
