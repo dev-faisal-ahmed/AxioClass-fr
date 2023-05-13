71efb2d7d1d0965c01e1157897c0b7c839fe7f50
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2023-05-14 02:17:13 +0600

    global layout issue fixed

--- src/layout/global_layout/GlobalLayout.jsx
+++ src/layout/global_layout/GlobalLayout.jsx
@@ -7,12 +7,12 @@ const GlobalLayout = ({ body, side }) => {
   return (
     <section className="bg-primary-50 h-screen grid grid-cols-[280px_1fr_400px]">
       <Sidebar />
-      <section>
+      <section className="h-full overflow-y-auto grid grid-rows-[auto_1fr]">
         {/* for top bar middle */}
         <TopBarMiddle />
         <section className="overflow-y-auto px-5">{body}</section>
       </section>
-      <section className="bg-white">
+      <section className="bg-white h-full overflow-y-auto grid grid-rows-[auto_1fr]">
         {/* for top bar right */}
         <TopBarSide />
         <section className="overflow-y-auto px-5">{side}</section>
