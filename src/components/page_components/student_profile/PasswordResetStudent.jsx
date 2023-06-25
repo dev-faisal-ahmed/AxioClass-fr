9087563309f01b46b307272006ee83be6e22909b
Os-Traveller <faisal.ahmed.20.35.197@gmail.com>
2023-06-25 17:12:14 +0600

    password reset completed

--- src/components/page_components/student_profile/PasswordResetStudent.jsx
+++ src/components/page_components/student_profile/PasswordResetStudent.jsx
@@ -1,16 +1,58 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { studentIdContext } from "../../../context_api/StudentIdProvider";
+import { serverAddress } from "../../../data/serverAddress";
+import { putReq } from "../../../utils/putReq";
+import { toast } from "react-hot-toast";
+import { toastConfig } from "../../../utils/toastConfig";
+import PasswordResetPdf from "./PasswordResetPdf";
 
-const PasswordResetStudent = () => {
+const PasswordResetStudent = ({ setPassModalTitle, setModalState }) => {
   const [confirmed, setConfirmed] = useState();
+  const { studentId } = useContext(studentIdContext);
+  const [pdfData, setPdfData] = useState({});
+
+  useEffect(() => {
+    setPassModalTitle("Are you sure?");
+  }, []);
+
+  const passReset = () => {
+    setConfirmed(true);
+    setPassModalTitle("Your Password");
+    const url = `${serverAddress}/modification/password`;
+    fetch(url, putReq({ id: studentId, role: "student" }))
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.okay) {
+          setPdfData(res.data);
+          console.log(res.data);
+        } else {
+          toast.error(res.msg, toastConfig);
+        }
+      });
+  };
   return (
     <>
       {confirmed ? (
-        ""
+        <PasswordResetPdf
+          image={pdfData.image}
+          name={pdfData.name}
+          id={pdfData.id}
+          password={pdfData.password}
+        />
       ) : (
-        <div>
-          <h1> Are you sure?</h1>
-          <button>Yes</button>
-          <button>No</button>
+        <div className="flex gap-2 justify-end">
+          <button
+            onClick={() => setModalState(false)}
+            className="px-5 py-1 animation rounded-md hover:bg-gray-400 hover:text-white"
+          >
+            No
+          </button>
+          <button
+            onClick={passReset}
+            className={`bg-primary-500 text-white px-5 py-1 rounded-md border-2 border-primary-500 hover:bg-transparent hover:text-primary-500 animation`}
+          >
+            Yes
+          </button>
         </div>
       )}
     </>
