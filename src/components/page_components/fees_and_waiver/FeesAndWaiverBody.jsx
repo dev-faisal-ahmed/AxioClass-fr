c5740a5b4437784f5fed478ec8b227530b75b351
Os-Traveller <ost.faisal.ahmed@gmail.com>
2023-07-15 02:46:46 +0600

    student-fees-page updated

--- src/components/page_components/fees_and_waiver/FeesAndWaiverBody.jsx
+++ src/components/page_components/fees_and_waiver/FeesAndWaiverBody.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import FeesGraph from "./FeesGraph";
 import FeesSummarySemesterWise from "./FeesSummarySemesterWise";
+import { serverAddress } from "../../../data/serverAddress";
+import { getLocalUser } from "../../../utils/localStorage";
+import { toast } from "react-hot-toast";
+import { toastConfig } from "../../../utils/toastConfig";
 
 const FeesAndWaiverBody = () => {
+  const { id } = getLocalUser();
+  const [feesInfo, setFeesInfo] = useState({});
+
+  useEffect(() => {
+    const url = `${serverAddress}/get-student/fees/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.okay) {
+          setFeesInfo(res.data);
+        } else {
+          toast.error(res.msg, toastConfig);
+        }
+      });
+  }, [id]);
+
   return (
     <section>
-      <FeesSummarySemesterWise />
+      <FeesSummarySemesterWise stat={feesInfo?.stat} />
       <FeesGraph />
     </section>
   );
